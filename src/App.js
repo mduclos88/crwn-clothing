fe6e0679508085624b7baf5ad2ec2121ec0c347d
Martin Duclos <martin.duclos@outlook.com>
2020-09-11 12:21:53 -0300

    added sign up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./componets/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.ultil";
+import { auth, createUserProfileDocument } from "./firebase/firebase.ultil";
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +20,28 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userRef = await createUserProfileDocument(user);
+
+        userRef.onSnapshot((userSnapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: userSnapShot.id,
+                ...userSnapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({
+          currentUser: user,
+        });
+      }
     });
   }
 
